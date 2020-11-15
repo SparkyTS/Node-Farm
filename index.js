0f914a0ba3894c1af436c4fd28564db720164a43
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-15 23:34:26 +0530

    tested file read write

--- index.js
+++ index.js
@@ -1,2 +1,10 @@
 str = 'hello world'
 console.log(str)
+
+const fs = require('fs');
+
+const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+console.log(textIn)
+
+fs.writeFileSync('./txt/output.txt', `Last updated date : ${new Date()}`)
+console.log('file written')
