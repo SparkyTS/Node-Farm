581484b347930dfde9f29b8961c79df0fa882bc6
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-15 23:54:22 +0530

    tested file read write Asynchronous way

--- index.js
+++ index.js
@@ -3,8 +3,23 @@ console.log(str)
 
 const fs = require('fs');
 
-const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
+//Blocking, Synchronous way
+/*const textIn = fs.readFileSync('./txt/input.txt', 'utf-8')
 console.log(textIn)
-
 fs.writeFileSync('./txt/output.txt', `Last updated date : ${new Date()}`)
-console.log('file written')
+console.log('file written')*/
+
+//Non-blocking , Asynchronous way
+fs.readFile( './txt/startr.txt', 'utf-8', (err, data1) => {
+    if(err) return console.log('error 💥')
+    fs.readFile( `./txt/${data1}.txt`, 'utf-8', (err, data2) => {
+        console.log(data2)
+        fs.readFile( `./txt/append.txt`, 'utf-8', (err, data3) => {
+            console.log(data3);
+            fs.writeFile('./txt/final.txt', `${data2}\n${data3}`, 'utf-8', err => {
+                console.log('Data has been written')
+            })
+        })
+    })
+})
+console.log('will read file!')
