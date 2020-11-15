a4a93e4d3c5f859a0c60b6b1497a5860b67a8692
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-16 03:05:29 +0530

    created over view section

--- index.js
+++ index.js
@@ -33,6 +33,24 @@ http = require('http');
 url = require('url');
 fs = require('fs');
 
+const replaceTemplate = (temp, product) => {
+    let output = temp.replace(/{%ID%}/g, product.id);
+    output = output.replace(/{%PRODUCT_NAME%}/g, product.productName);
+    output = output.replace(/{%IMAGE%}/g, product.image);
+    output = output.replace(/{%PRICE%}/g, product.price);
+    output = output.replace(/{%FROM%}/g, product.from);
+    output = output.replace(/{%NUTRIENTS%}/g, product.nutrients);
+    output = output.replace(/{%QUANTITY%}/g, product.quantity);
+    output = output.replace(/{%DESCRIPTION%}/g, product.description);
+
+    if(!product.organic) output = output.replace(/{%NOT_ORGANIC%}/g, 'not-organic');
+    return output;
+}
+
+const tempOverView = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
+const tempProduct = fs.readFileSync(`${__dirname}/templates/template-product.html`, 'utf-8');
+
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
@@ -40,19 +58,29 @@ const server = http.createServer((req, res) => {
     const pathName = req.url;
 
     switch (pathName) {
+        //Overview Page
         case '/':
         case '/overview':
-            res.end('This is the overview');
+            res.writeHead(200, {'Content-Type': 'text/html'});
+
+            const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+            res.end(tempOverView.replace('{%PRODUCT_CARDS%}', cardsHtml));
             break
+
+        //Product Page
         case '/product':
             res.end('This is the porduct');
             break;
+
+        //API
         case '/api':
             res.writeHead(200, {
                 'Content-Type': 'application/json'
             })
             res.end(data);
             break;
+
+        //Not found
         default:
             res.writeHead(404, {
                 'Content-type': 'text/html',
