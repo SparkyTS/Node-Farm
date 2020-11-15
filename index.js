1d364b44c0290265fe4428348bd1750ea7993c54
Tanay <48762037+SparkyTS@users.noreply.github.com>
2020-11-16 03:14:35 +0530

    created product page

--- index.js
+++ index.js
@@ -55,9 +55,9 @@ const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
 const server = http.createServer((req, res) => {
-    const pathName = req.url;
+    const { query, pathname} = url.parse(req.url, true);
 
-    switch (pathName) {
+    switch (pathname) {
         //Overview Page
         case '/':
         case '/overview':
@@ -69,7 +69,9 @@ const server = http.createServer((req, res) => {
 
         //Product Page
         case '/product':
-            res.end('This is the porduct');
+            const product = dataObj[query.id];
+            res.writeHead(200, {'Content-Type': 'text/html'});
+            res.end(replaceTemplate(tempProduct, product));
             break;
 
         //API
